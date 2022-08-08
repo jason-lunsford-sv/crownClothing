3efb8a95ee306235aec53c814c43678a24e2d758
Jason Lunsford <jason.lunsford@softvision.com>
2022-08-08 12:06:34 -0400

    Added Redux Persist to afford rehydrating select aspects of the store from local storage on reload

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,19 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
+
+// Redux Persist init config
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+};
+
+// Create persistedReducer, this allows us to rehydrate the reducers from local storage
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // Add middleware (basically enhancers to the redux store) to this array
 const middleWares = [logger];
@@ -11,4 +23,5 @@ const middleWares = [logger];
 // to be executed BEFORE the reducers are executed.
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+export const persistor = persistStore(store);
