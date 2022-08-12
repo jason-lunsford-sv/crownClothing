870a7526d008c347ceb57bb26427084234bb38e7
Jason Lunsford <jason.lunsford@softvision.com>
2022-08-12 15:18:00 -0400

    Converted Firebase connection values into env variables for better security

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiGwM9Kze1D8ApHlx33voXpYgeYwoZNTI",
-    authDomain: "crwn-clothing-tutorial-db.firebaseapp.com",
-    projectId: "crwn-clothing-tutorial-db",
-    storageBucket: "crwn-clothing-tutorial-db.appspot.com",
-    messagingSenderId: "462113948447",
-    appId: "1:462113948447:web:8ef233a1b5d39eba48a62a"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
   
 const firebaseApp = initializeApp(firebaseConfig);
